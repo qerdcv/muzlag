008f39e00ab89c1a3d82ddef3b97916a85c3eb7a
qerdcv <vadym.tishchenko2@gmail.com>
2024-02-29 21:27:32 +0200

    Setup metrics in dirty way

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"golang.org/x/sync/errgroup"
 
 	"github.com/qerdcv/muzlag.go/internal/bot"
 	"github.com/qerdcv/muzlag.go/internal/bot/handler"
@@ -31,19 +37,44 @@ func main() {
 		return
 	}
 
-	l.Info("starting")
-	if err = b.Run(); err != nil {
-		l.Error("bot run", "err", err)
-		return
+	ctx := context.Background()
+	signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+
+	s := http.Server{
+		Addr: ":8009",
 	}
 
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
-	<-quit
+	errG, ctx := errgroup.WithContext(ctx)
+	l.Info("starting")
+	errG.Go(b.Run)
+	errG.Go(func() error {
+		http.Handle("/metrics", promhttp.Handler())
+		if serveErr := s.ListenAndServe(); serveErr != nil && !errors.Is(err, http.ErrServerClosed) {
+			return fmt.Errorf("http listen and serve: %w", serveErr)
+		}
 
-	fmt.Println("shutting down")
+		return nil
+	})
+
+	<-ctx.Done()
+
+	l.Info("shutting down")
+
 	if err = b.Close(); err != nil {
 		l.Error("bot close", "err", err)
 		return
+	}
+
+	cancelCtx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	go func() {
+		<-cancelCtx.Done()
+		if closeErr := s.Close(); closeErr != nil {
+			l.Error("server close", "err", closeErr)
+		}
+	}()
+
+	if err = s.Shutdown(cancelCtx); err != nil {
+		l.Error("server shutdown: %w", err)
 	}
 }
--- go.mod
+++ go.mod
@@ -6,18 +6,26 @@ require (
 	github.com/bwmarrin/discordgo v0.27.1
 	github.com/jonas747/dca v0.0.0-20210930103944-155f5e5f0cc7
 	github.com/kkdai/youtube/v2 v2.10.0
+	github.com/prometheus/client_golang v1.19.0
+	golang.org/x/sync v0.3.0
 )
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/bitly/go-simplejson v0.5.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dlclark/regexp2 v1.10.0 // indirect
 	github.com/dop251/goja v0.0.0-20240220182346-e401ed450204 // indirect
 	github.com/go-sourcemap/sourcemap v2.1.3+incompatible // indirect
 	github.com/google/pprof v0.0.0-20240207164012-fb44976bdcd5 // indirect
 	github.com/gorilla/websocket v1.5.1 // indirect
 	github.com/jonas747/ogg v0.0.0-20161220051205-b4f6f4cf3757 // indirect
+	github.com/prometheus/client_model v0.5.0 // indirect
+	github.com/prometheus/common v0.48.0 // indirect
+	github.com/prometheus/procfs v0.12.0 // indirect
 	golang.org/x/crypto v0.19.0 // indirect
 	golang.org/x/net v0.21.0 // indirect
 	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/protobuf v1.32.0 // indirect
 )
