19098db6024c3321c130708babf9bbdefa65e228
qerdcv <vadym.tishchenko2@gmail.com>
2024-03-02 09:40:35 +0200

    Refactor metrics handler runner

--- cmd/main.go
+++ cmd/main.go
@@ -2,14 +2,10 @@ package main
 
 import (
 	"context"
-	"errors"
-	"fmt"
-	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"golang.org/x/sync/errgroup"
 
 	"github.com/qerdcv/muzlag.go/internal/bot"
@@ -17,6 +13,7 @@ import (
 	"github.com/qerdcv/muzlag.go/internal/bot/player"
 	"github.com/qerdcv/muzlag.go/internal/config"
 	"github.com/qerdcv/muzlag.go/internal/logger"
+	"github.com/qerdcv/muzlag.go/internal/metrics"
 )
 
 func main() {
@@ -40,41 +37,17 @@ func main() {
 	ctx := context.Background()
 	signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
 
-	s := http.Server{
-		Addr: ":8009",
-	}
-
 	errG, ctx := errgroup.WithContext(ctx)
 	l.Info("starting")
 	errG.Go(b.Run)
 	errG.Go(func() error {
-		http.Handle("/metrics", promhttp.Handler())
-		if serveErr := s.ListenAndServe(); serveErr != nil && !errors.Is(err, http.ErrServerClosed) {
-			return fmt.Errorf("http listen and serve: %w", serveErr)
-		}
-
-		return nil
+		return metrics.RunMetricsHandler(ctx, l)
 	})
 
 	<-ctx.Done()
 
-	l.Info("shutting down")
-
 	if err = b.Close(); err != nil {
 		l.Error("bot close", "err", err)
 		return
-	}
-
-	cancelCtx, cancel := context.WithCancel(ctx)
-	defer cancel()
-	go func() {
-		<-cancelCtx.Done()
-		if closeErr := s.Close(); closeErr != nil {
-			l.Error("server close", "err", closeErr)
-		}
-	}()
-
-	if err = s.Shutdown(cancelCtx); err != nil {
-		l.Error("server shutdown: %w", err)
 	}
 }
---
+++ internal/metrics/runner.go
@@ -0,0 +1,47 @@
+package metrics
+
+import (
+	"context"
+	"errors"
+	"log/slog"
+	"net/http"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+
+	"github.com/qerdcv/muzlag.go/internal/logger"
+)
+
+func RunMetricsHandler(ctx context.Context, l logger.Logger[*slog.Logger]) error {
+	mux := http.NewServeMux()
+	mux.Handle("/metrics", promhttp.Handler())
+	s := http.Server{
+		Addr:    "8009",
+		Handler: mux,
+	}
+
+	go func() {
+		<-ctx.Done()
+
+		cancelCtx, cancel := context.WithTimeout(ctx, 10*time.Second)
+		defer cancel()
+
+		go func() {
+			<-cancelCtx.Done()
+			if err := s.Close(); err != nil {
+				l.Error("server close", "err", err)
+			}
+		}()
+
+		if err := s.Shutdown(cancelCtx); err != nil {
+			l.Error("server shutdown", "err", err)
+		}
+	}()
+
+	l.Info("running metrics server")
+	if err := s.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		l.Error("metrics listen and serve", "err", err)
+	}
+
+	return nil
+}
